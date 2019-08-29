4697f22614d0f15d54dc2be6b25b48fba8c14aef
kubo158 <kubo158@hotmail.com>
2019-08-30 08:19:18 +0900

    190830 kadai2.js

--- kadai2.js
+++ kadai2.js
@@ -1,94 +1,130 @@
 'use strict'; {
 
+  // ラジオボタンの要素を取得
+  const radioAll = document.getElementById('radioAll');
+  const radioDoing = document.getElementById('radioDoing');
+  const radioComplete = document.getElementById('radioComplete');
+
+
   //追加ボタンの要素を取得
-  let addButton = document.getElementById('addButton');
+  const addButton = document.getElementById('addButton');
 
   //１．<iuput>タグを取得する
-  let todoContent = document.getElementById('todoContent');
+  const todoContent = document.getElementById('todoContent');
 
   //２．<tbody>タグを取得する
-  let todoList = document.getElementById('todoList');
+  const todoList = document.getElementById('todoList');
 
-  // IDの初期値
-  let countUpValue = 0;
 
+  // id初期値
+  let num = 0;
 
 
 
   //追加ボタンを押す
   addButton.addEventListener('click', () => {
 
-  //Inputnの入力値を取得
-  let todoContentValue = todoContent.value;
-  if (todoContentValue =='') {
-    return false;
-  }
+    //上記、生成した要素の配列化
+    const todos = [];
+    const task = {};
+
+
+    //ラジオボタンを押す
+    radioAll.addEventListener('change', () => {   //全て
+    });
+
+    radioDoing.addEventListener('change', () => {   //作業中
+      const progress = todos.filter(x => x.status === '作業中')
+      console.log(progress);
+    });
+
+    radioComplete.addEventListener('change', () => {   //完了
+      const complete = todos.filter(x => x.status === '完了')
+      console.log(complete);
+
+    });
+
+    //Inputnの入力値を取得
+    const todoContentValue = todoContent.value;
+
+
+    task.id = num;
+    task.comment = todoContentValue;
+    task.status = '作業中';
+    todos.push(task);
+    console.log(task);
+
+
+
+    if (todoContentValue == '') {
+      return false;
+    }
 
     //tr エレメントを新規作成(ただ生成するだけ)
     let tr = document.createElement('tr');
-          tr.classList.add('addTr');
+    tr.classList.add('addTr');
 
-      //td エレメントを新規作成(ただ生成するだけ)
-      let td = document.createElement('td');
-      td.classList.add('id');
-      //tdの中に入れたいモノをセット
-      td.textContent = (countUpValue);
-      countUpValue++;
-      //生成したtdをtrにセット
-      tr.appendChild(td);
-
-
-      //td エレメントを新規作成(ただ生成するだけ)
-      td = document.createElement('td');
-      td.classList.add('comment');
-      //tdの中に入れたいモノをセット
-      td.textContent = (todoContentValue);
-      //生成したtdをtrにセット
-      tr.appendChild(td);
+    //td idエレメントを新規作成(ただ生成するだけ)
+    let td = document.createElement('td');
+    td.classList.add('id');
+    //tdの中に入れたいモノをセット
+    td.textContent = (num);
+    num++;
+    //生成したtdをtrにセット
+    tr.appendChild(td);
 
 
-
-      td = document.createElement('td');
-      td.classList.add('state');
-      //ボタンをセット
-      let btn = document.createElement('button');
-      btn.textContent = ('作業中');
-      td.appendChild(btn);
-      tr.appendChild(td);
-
-
-      td = document.createElement('td');
-      td.classList.add('button');
-      let removeBtn = document.createElement('button');
-      removeBtn.textContent = ('削除');
-      td.appendChild(removeBtn);
-      tr.appendChild(td);
+    //td エレメントを新規作成(ただ生成するだけ)
+    td = document.createElement('td');
+    td.classList.add('comment');
+    //tdの中に入れたいモノをセット
+    td.textContent = (todoContentValue);
+    //生成したtdをtrにセット
+    tr.appendChild(td);
 
 
-    //tr エレメントをtbody内に追加(ここではじめて表示される)
-    todoList.appendChild(tr);
 
-    //上記、生成した要素の配列化
-    let todo = [];
-    todo.push('.id', '.comment', 'btn', 'removeBtn');
-    console.log(todo);
+    td = document.createElement('td');
+    td.classList.add('state');
+    //ボタンをセット
+    const btn = document.createElement('button');
+    btn.textContent = (task.status);
+    td.appendChild(btn);
+    tr.appendChild(td);
 
 
     btn.addEventListener('click', () => {
-      if(btn.textContent === ('作業中')) {
-      btn.textContent = ('完了');
-    } else {
-      btn.textContent = ('作業中');
-    }
+      switch (task.status) {
+        case '作業中':
+          task.status = '完了';
+          console.log(task);
+          btn.textContent = (task.status);
+          break;
+        case '完了':
+          task.status = '作業中';
+          btn.textContent = (task.status);
+          break;
+      }
     });
 
+
+
+
+    td = document.createElement('td');
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = ('削除');
+    td.appendChild(removeBtn);
+    tr.appendChild(td);
+
+    //tr エレメントをtbody内に追加(ここではじめて表示される)
+    todoList.appendChild(tr);
     removeBtn.addEventListener('click', () => {
-      //要素が無くなった時は、カウントを0に戻したい。
-      if(tr === null) {
-        countUpValue = 0;}
       const element1 = document.querySelector('.addTr');
-        element1.parentNode.removeChild(element1);
-    });
+      element1.parentNode.removeChild(element1);
+  });
 
   });
+
+
+  // use strictの終わり
 }
