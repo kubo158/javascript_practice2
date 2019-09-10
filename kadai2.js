9a2c69a42e94e94ea4713b2fc191add7a7e3c3d2
kubo158 <kubo158@hotmail.com>
2019-09-10 17:43:17 +0900

    190910 kadai2.js

--- kadai2.js
+++ kadai2.js
@@ -5,76 +5,97 @@
   //＊＊＊＊＊　Dom開始　＊＊＊＊＊
   window.addEventListener('DOMContentLoaded', () => {
 
-    //１．<iuput>タグを取得する
+    //<iuput>タグを取得する
     const inputContent = document.getElementById('inputContent');
 
-    //２．<tbody>タグを取得する
+    //<tbody>タグを取得する
     const todoList = document.getElementById('todoList');
 
 
     //追加ボタンの要素を取得
     const addButton = document.getElementById('addButton');
 
+    //createElement
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+
+    //配列
     const todos = [];
+    let task = {};
 
-    //＊＊＊＊＊　追加ボタンイベントを押す　＊＊＊＊＊
-    addButton.addEventListener('click', () => {
-
-      const id = todos.length;
-      const comment = inputContent.value;
-      let switchBtn = '作業中';
-      const dlt = '削除';
-
-      let task = {
-        id,
-        comment,
-        switchBtn,
-        dlt
-      };
-
-      todos.push(task);
-
-      console.log(todos);
-      // todos.forEach((item, index) => {
-      //   console.log(`${index}: ${item}`);
-      // });
+    //ボタン
+    let switchBtn = '作業中';
+    const dlt = '削除';
 
 
-      //▲▲▲▲　tr エレメントを新規作成(ただ生成するだけ)　▲▲▲▲
-      let tr = document.createElement('tr');
-      tr.classList.add('work');
+    //○○○○○○○○  todoボタン作成処理  ○○○○○○○○//
+const createTodoList = function(){
 
-      //---- td idエレメントを新規作成 ----
-      let td = document.createElement('td');
-      //tdの中に入れたいモノをセット
-      td.textContent = id;
-      //生成したtdをtrにセット
-      tr.appendChild(td);
-      //---- td idエレメント終わり ----
+  const id = todos.length;
+  const comment = inputContent.value;
+
+  task = {
+    id,
+    comment,
+    switchBtn,
+    dlt
+  };
+
+  todos.push(task);
+
+  console.log(todos);
+
+  //▲▲▲▲　tr エレメントを新規作成(ただ生成するだけ)　▲▲▲▲
+  tr = document.createElement('tr');
+  tr.classList.add('work');
+
+  //---- td idエレメントを新規作成 ----
+  td = document.createElement('td');
+  //tdの中に入れたいモノをセット
+  td.textContent = id;
+  //生成したtdをtrにセット
+  tr.appendChild(td);
+  //---- td idエレメント終わり ----
 
 
-      //---- td inputした内容を新規作成 ----
-      td = document.createElement('td');
-      //tdの中に入れたいモノをセット
-      if (comment == '') {
-        return false;
-      } else {
-        td.textContent = comment;
-      }
-      //生成したtdをtrにセット
-      tr.appendChild(td);
-      //---- td 入力した内容終わり ----
+  //---- td inputした内容を新規作成 ----
+  td = document.createElement('td');
+  //tdの中に入れたいモノをセット
+  if (comment == '') {
+    return false;
+  } else {
+    td.textContent = comment;
+  }
+  //生成したtdをtrにセット
+  tr.appendChild(td);
+  //---- td 入力した内容終わり ----
 
 
-      //---- td 作業中ボタンを新規作成 ----
+  //---- td 作業中ボタンを新規作成 ----
+  createWorkBtn();
+  tr.appendChild(td);
+  //---- td 作業中ボタン終わり ----
+
+
+  //---- td 削除を新規作成 ----
+  createRemoveBtn();
+  tr.appendChild(td);
+  //---- td 削除ボタン終わり ----
+
+  return task;
+}
+    //○○○○○○○○  Todoボタン終わり  ○○○○○○○○//
+
+
+
+    //○○○○○○○○  作業中ボタン作成処理  ○○○○○○○○//
+    const createWorkBtn = function(){
       td = document.createElement('td');
       //ボタンをセット
       const btn = document.createElement('button');
       btn.textContent = (switchBtn);
+      // btn.classList.add('change');
       td.appendChild(btn);
-      tr.appendChild(td);
-      //---- td 作業中ボタン終わり ----
-
 
       //●●●●　作業中イベントボタン　●●●●
       btn.addEventListener('click', (e) => {
@@ -100,22 +121,23 @@
             }
             console.log(todos);
             break;
+            default:
+            break;
         }
       });
       //●●●●　作業中イベントボタン終わり　●●●●
+      return btn;
+    }
+    //○○○○○○○○  作業中ボタン終わり  ○○○○○○○○//
 
+    //○○○○○○○○  削除ボタン作成処理  ○○○○○○○○//
+    const createRemoveBtn = function(){
 
-      //---- td 削除を新規作成 ----
       td = document.createElement('td');
       const removeBtn = document.createElement('button');
       removeBtn.textContent = (dlt);
+      removeBtn.classList.add('remove');
       td.appendChild(removeBtn);
-      tr.appendChild(td);
-      //---- td 削除ボタン終わり ----
-
-      todoList.appendChild(tr);
-      //▲▲▲▲　エレメントをtbody内に追加　▲▲▲▲
-
 
       //●●●●　削除イベントボタン　●●●●
       removeBtn.addEventListener('click', (e) => {
@@ -127,6 +149,18 @@
         console.log(todos);
       });
       //●●●●　削除イベントボタン終わり　●●●●
+        return removeBtn;
+}
+    //○○○○○○○○  削除ボタン終わり  ○○○○○○○○//
+
+
+
+    //＊＊＊＊＊　追加ボタンイベントを押す　＊＊＊＊＊
+    addButton.addEventListener('click', () => {
+
+      createTodoList();
+      todoList.appendChild(tr);
+      //▲▲▲▲　エレメントをtbody内に追加　▲▲▲▲
 
 
 
